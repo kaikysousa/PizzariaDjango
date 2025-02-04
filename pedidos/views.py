9262acd4a18570cc7e7504dc6d893f113cdcf75b
kaikysousa <kaikydsousa@gmail.com>
2025-02-04 16:17:07 -0300

    Feature de Carrinho para o pedido

--- pedidos/views.py
+++ pedidos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Produto
+from django.core.paginator import Paginator
 # Create your views here.
 
 def index(request):
@@ -8,4 +9,9 @@ def index(request):
     item_name = request.GET.get('item_name')
     if item_name != '' and item_name is not None:
         produto_objetos = produto_objetos.filter(nome__icontains=item_name)
+
+    #paginação
+    paginator = Paginator(produto_objetos, 4)
+    page = request.GET.get('page')
+    page_obj = paginator.get_page(page)
     return render(request, 'pedidos/index.html', {'produto_objetos': produto_objetos})
