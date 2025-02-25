c6c0c68b97ec5db77d3bea4a9a4e7863a47a83bf
kaikysousa <kaikydsousa@gmail.com>
2025-02-25 16:19:35 -0300

    Feature botão adicionar meia para produtos da categoria pizza

--- DjangoProject/urls.py
+++ DjangoProject/urls.py
@@ -19,8 +19,11 @@ from django.urls import path
 from pedidos import views
 from django.conf.urls import static
 from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('checkout/', views.checkout, name='checkout'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
